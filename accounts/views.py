0421856b78e58a94a59f6b6c889d5e16272ec982
Azmain Mahtab <azmainmahtab97@gmail.com>
2021-05-25 06:24:38 +0600

    login view prototype working  working

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import redirect, render
 from .forms import CustomerForm, ProductForm, OrderForm, CreateUserForm
 from django.contrib.auth.forms import UserCreationForm
 from .filters import OrderFilter
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 from .models import *
 
 # Create your views here.
@@ -160,7 +162,20 @@ def remove_customer(request, pk):
 
 ##### Authentications #####
 
-def login(request):
+def login_view(request):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request,"Username or password is incorrect")
+
     return render(request, 'accounts/login.html')
 
 def register(request):
@@ -170,6 +185,9 @@ def register(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'account was created successfuly')
+
+            return redirect('login')
 
     context = {
         'form': form,
